766a8aa8d943b5bb08fc4aaa963c61d879410bc4
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-05-21 10:50:52 +0200

    reorganize css

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
-import './utility/customstyle.css'
 import AppRouter from './utility/router.jsx'
 
 createRoot(document.getElementById('root')).render(
