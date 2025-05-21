de4e80478f3cf0ee5fa6a86a05991fd5ae0c395c
Magnus2-kodehode <magnus2.kodehode@gmail.com>
2025-05-21 10:57:16 +0200

    game cards

--- FrontEnd/src/pages/homepage.jsx
+++ FrontEnd/src/pages/homepage.jsx
@@ -5,6 +5,20 @@ const HomePage = () => {
     <div className='main'>
       <h1>Welcome to the Home Page</h1>
       <p>This is the content of the home page.</p>
+      <div className='game-list'>
+        <div className='game-card'>
+          <div className='game-thumbnail'></div>
+          <div className='game-title'>Game title</div>
+        </div>
+        <div className='game-card'>
+          <div className='game-thumbnail'></div>
+          <div className='game-title'>Game title</div>
+        </div>
+        <div className='game-card'>
+          <div className='game-thumbnail'></div>
+          <div className='game-title'>Game title</div>
+        </div>
+      </div>
     </div>
   )
 }
