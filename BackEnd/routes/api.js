9fb15e4bd42597fcad2eb2722eb60495bee3c3f0
bryan-kodehode <bryan.kodehode@gmail.com>
2025-05-26 14:25:03 +0200

    Backend update
    deletes pin code

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -6,11 +6,13 @@ import { register } from "../controllers/register.js";
 import { login } from "../controllers/login.js";
 import { checkPin } from "../controllers/checkpin.js";
 import { createpin } from "../controllers/createpin.js";
+import { deletePin } from "../controllers/deletepin.js";
 
 // Routes
 router.post("/api/player/register", register);
 router.post("/api/player/login", login);
 router.post("/api/game/createpin", createpin);
 router.get("/api/game/checkpin/:pin", checkPin);
+router.delete("/api/game/deletepin/:pin", deletePin);
 
 export default router;
