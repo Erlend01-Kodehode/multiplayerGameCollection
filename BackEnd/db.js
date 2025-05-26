567a589239189b555b841bc0c9864c080e703f42
bryan-kodehode <bryan.kodehode@gmail.com>
2025-05-26 10:14:44 +0200

    Update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,8 +1,13 @@
 import sqlite3 from "sqlite3";
+import { resolve } from "path";
 
-const db = new sqlite3.Database("./db.sqlite", (err) => {
-  if (err) console.error("Could not connect to database", err);
-  else console.log("Connected to SQLite database.");
+const dbPath = resolve("db.sqlite");
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("Failed to connect to SQLite:", err.message);
+  } else {
+    console.log("Connected to SQLite database.");
+  }
 });
 
 export default db;
