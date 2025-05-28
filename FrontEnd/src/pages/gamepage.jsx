dcb3557d1152ead5cfa8a3b6a2dc6b26588a5200
Tor-HodeKode <tor.kodehode@gmail.com>
2025-05-28 09:19:49 +0200

    many
    Many

--- FrontEnd/src/pages/gamepage.jsx
+++ FrontEnd/src/pages/gamepage.jsx
@@ -1,12 +1,1 @@
-import { Outlet } from "react-router-dom";
 
-import styles from "../CSSModule/game.module.css";
-
-const GamePage = () => {
-    return (
-        <div className={styles.gamepage}>
-            <Outlet />
-        </div>
-    );
-}
-export default GamePage;
